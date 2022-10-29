16590bdced87e91854ef83816315103f24adb627
Iván <93777037+ivandezra@users.noreply.github.com>
2022-10-29 10:12:58 +0200

    Update UI.py

--- UI.py
+++ UI.py
@@ -5,7 +5,7 @@ import random
 #alphabet
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-with open("corncob_lowercase.txt") as file:
+with open("58110") as file:
     lines = file.readlines()
     lines = [line.rstrip() for line in lines]
 
